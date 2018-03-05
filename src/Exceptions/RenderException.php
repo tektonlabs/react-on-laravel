aefc44fa057d49fbcc63ca3f5bff3dfdcf366476
Jehu Pacheco <jehu.pacheco@tektonlabs.com>
2018-03-05 14:23:43 -0500

    Throw exception in renderer

--- src/Exceptions/RenderException.php
+++ src/Exceptions/RenderException.php
@@ -4,8 +4,9 @@ namespace TektonLabs\ReactOnLaravel\Exceptions;
 
 class RenderException extends \Exception
 {
-    public function __construct($message)
+    public function __construct($componentName, $consoleReplay)
     {
+        $message = 'Error rendering component '.$componentName."\nConsole log:".$consoleReplay;
         parent::__construct($message);
     }
 }
