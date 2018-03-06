2059d885e35c8b0faab47372e6cb0ba5fb82edbb
Jehu Pacheco <jehu.pacheco@tektonlabs.com>
2018-03-06 10:57:46 -0500

    Better exception message

--- src/Exceptions/RenderException.php
+++ src/Exceptions/RenderException.php
@@ -6,7 +6,7 @@ class RenderException extends \Exception
 {
     public function __construct($componentName, $html)
     {
-        $message = 'Error rendering component '.$componentName."\n".$html;
+        $message = 'Error rendering component '.$componentName."\n".html_entity_decode($html, ENT_QUOTES);
         parent::__construct($message);
     }
 }
